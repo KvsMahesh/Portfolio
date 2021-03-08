fd5f964b70c9a1293254f3991a023e4de580df1b
Mahesh Kvs <51854557+KvsMahesh@users.noreply.github.com>
2021-03-08 15:21:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,6 +10,6 @@ window.addEventListener("scroll",function(){
     var nav = document.querySelector("nav");
     nav.classList.toggle("sticky", window.scrollY > 0 );
     })
-window.addEventListener('load',function(){
-    document.querySelector('.load-wrap').style.display = 'none';
-});
+window.addEventListener('load',
+    setTimeout(function(){document.querySelector('.load-wrap').style.display = 'none';},3000)
+);
